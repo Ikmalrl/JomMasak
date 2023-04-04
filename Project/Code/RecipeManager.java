6c7566b7996717989226b66edbc4fc72c26b6760
Ikmal Iman <126444992+Ikmalrl@users.noreply.github.com>
2023-04-04 21:08:16 +0800

    Update RecipeManager.java

--- Project/Code/RecipeManager.java
+++ Project/Code/RecipeManager.java
@@ -4,6 +4,7 @@ import java.util.*;
 public class RecipeManager {
     public static List<Recipe> searchRecipes(String[] ingredients) throws SQLException {
 
+        // SQL Query
         String sql = "SELECT r.recipe_name, (SELECT GROUP_CONCAT(i.ingredient_name SEPARATOR ', ') FROM recipe_ingredients ri JOIN ingredients i ON ri.ingredient_id = i.ingredient_id WHERE ri.recipe_id = r.recipe_id) AS ingredients, " +
                 "GROUP_CONCAT(CONCAT(instr.step_number, '. ', instr.instruction_text) ORDER BY instr.step_number SEPARATOR '\n') AS instructions " +
                 "FROM recipes r " +
@@ -13,6 +14,7 @@ public class RecipeManager {
                 "WHERE i.ingredient_name IN (" + String.join(",", Collections.nCopies(ingredients.length, "?")) + ") " +
                 "GROUP BY r.recipe_id";
 
+        //Connect to database
         try(Connection conn = DatabaseConnector.getConnection();
             PreparedStatement pstmt = conn.prepareStatement(sql)) {
 
